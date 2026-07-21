d24699c5d49c070c6c29ccadb47682c9a423af9d
Auvishek Das <auvishekdas9@gmail.com>
2026-07-21 17:18:41 +0600

    Delete Myproject4 directory

--- Myproject4/views.py
+++
@@ -1,6 +0,0 @@
-from django.http import HttpResponse
-
-def main_home(request):
-    return HttpResponse("This is our Main module home page")
-def profile(request):
-    return HttpResponse("My Profile Page")
