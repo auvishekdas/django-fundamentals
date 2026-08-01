7c3b76d4e8c0ff51c5fed2afc9c5643e14faa8c4
Auvishek Das <auvishekdas9@gmail.com>
2026-08-01 23:03:06 +0600

    Add files via upload

---
+++ Myproject14/core/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+
+def home(request):
+    text = {
+        "name": "Auvishek Das",
+        "address": "Dhaka",
+        "phone": "254878",
+        "language": ['English','German']
+    }
+    return render(request,'index.html',text)
+
+def about(request):
+    return render(request,'about.html')
+
+def contact(request):
+    return render(request,'contact.html')
+
+def profile(request):
+    return render(request,'profile.html')
