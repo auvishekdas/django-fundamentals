ed9df26319e7ad97dbf2ab51767bdba1afc494c1
Auvishek Das <auvishekdas9@gmail.com>
2026-08-06 16:53:50 +0600

    Add files via upload

---
+++ Myproject18/index/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+class About(models.Model):
+    title = models.CharField(max_length=100, blank=False)
+    description = models.TextField(max_length=700, blank=False)
+    image = models.ImageField(upload_to='about/', blank=False)
