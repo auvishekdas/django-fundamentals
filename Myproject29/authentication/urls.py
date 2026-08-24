d21373d75dc9c639cfddafc2f0ca5a200056a979
Auvishek Das <auvishekdas9@gmail.com>
2026-08-24 17:12:47 +0600

    Add files via upload

---
+++ Myproject29/authentication/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.authlogin, name='login'),
+    path('registration/', views.authregistration, name='registration'),
+    path('forgot-password/', views.forgotpassword, name='forgotpassword'),
+]
---
+++ Myproject29/authentication/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate,login
+
+def authlogin(request):
+    if request.method == 'POST':
+        name=request.POST['name']
+        password=request.POST['password']
+        user=authenticate(request,username = name, password = password)
+        if user is not None:
+            login(request,user)
+            return redirect('profile')
+        else:
+            print('invalid username or password')
+    return render(request, 'authentication/login.html')
+
+def authregistration(request):
+    return render(request, 'authentication/registration.html')
+
+def forgotpassword(request):
+    return render(request, 'authentication/forgot.html')
